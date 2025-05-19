94b54529f7ca2569e391bf0b7bf06e54d242f6ac
Zaali <zaaliai25@gmail.com>
2025-05-19 23:12:27 +0000

    Fix: Correct Link children for Sign Up button on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -131,8 +131,10 @@ export default async function HomePage() {
           </p>
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
             <Button asChild size="lg" className="bg-primary hover:bg-primary/90 text-primary-foreground">
-              <Link href="/signup" legacyBehavior>
-                <UserPlus className="mr-2 h-5 w-5" /> Sign Up as a User
+              <Link href="/signup">
+                <>
+                  <UserPlus className="mr-2 h-5 w-5" /> Sign Up as a User
+                </>
               </Link>
             </Button>
             <Button asChild variant="outline" size="lg">
